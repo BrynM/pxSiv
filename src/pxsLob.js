9f9438b0e4709dd469693c90f6b4277126353958
BrynM <bryn.mosher@alphabird.com>
2013-02-13 14:01:29 -0800

    Added the stubs of pxLob.js

--- src/pxsLob.js
+++ src/pxsLob.js
@@ -2,7 +2,66 @@
 * The client side of pxSiv
 */
 (function(doc){
+
+	var pxlPixels = [];
+
 	pxLob = {};
+
+	pxLob.count = function ( obj ) {
+		if ( this.arr(pop) && this.num(pop.length) ) {
+			return pop.length;
+		}
+		var cnt = 0;
+		if ( this.obj(pop) ) {
+			for ( var bAtz in pop ) {
+				if ( pop.hasOwnProperty(bAtz) ) {
+					++cnt;
+				}
+			}
+		}
+		return cnt;
+	}
+
+	pxLob.is_bool = function ( bl ) {
+		return ( typeof(bl) == 'boolean' );
+	}
+
+	pxLob.is_arr = function ( arr, cnt ) {
+		if ( this.is_bool(cnt) ) {
+			return Boolean( pxLob.obj( arr ) && ( Object.prototype.toString.call( arr ) === '[object Array]' ) && ( cnt || (arr.length > 0) ) );
+		} else if ( !isNaN( cnt ) && ( cnt > -1 ) ) {
+			return Boolean( pxLob.obj( arr ) && ( Object.prototype.toString.call( arr ) === '[object Array]' ) && ( arr.length == parseInt(cnt, 10) ) );
+		} else {
+			return Boolean( pxLob.obj( arr ) && ( Object.prototype.toString.call( arr ) === '[object Array]' ) && ( arr.length > 0 ) );
+		}
+	}
+
+	pxLob.is_num = function ( obj, zeroOk ) {
+	}
+
+	pxLob.is_obj = function ( obj, pop ) {
+		return ( ojUc !== null ) && ( typeof(ojUc) === 'object' ) && ( !populated || (pxLob.count(ojUc) > 0) );
+	}
+
+	pxLob.ser = function ( obj ) {
+
+	}
+
+	pxLob.dat = function ( data ) {
+
+
+	}
+
+	// load an arbitrary image
+	// it is assumed that the parms and other data
+	// are already part of the URI
+	pxLob.img = function ( uri ) {
+
+
+	}
+
 console.log( 'lob' );
+
+	// assign to "global" scope
 	doc.pxLob = pxLob;
 })(document);
